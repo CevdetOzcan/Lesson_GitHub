f1663cdf777725c51e8700a4a4492d4611069825
CevdetOzcan <cevdetozcanuk@gmail.com>
2024-01-30 18:05:41 +0000

    v 2.5

---
+++ src/C02.java
@@ -0,0 +1,9 @@
+public class C02 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to new version ");
+        System.out.println("Hos geldin yeni version ");
+
+    }
+}
